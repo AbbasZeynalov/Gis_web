245107efdde446dd49963523c8d0d70f582999d6
Javid Mammadov <javidm@risk.az>
2019-11-07 10:47:34 +0400

    change dev server ip

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const API_DOMAIN_LOCAL = 'localhost';
-export const API_DOMAIN_DEV = '172.30.98.12';
+export const API_DOMAIN_DEV = '172.30.20.212';
 export const API_PORT = '3200';
 export const API_ENDPOINT_LOCAL = `http://${API_DOMAIN_LOCAL}:${API_PORT}/graphql`;
 export const API_ENDPOINT_DEV = `http://${API_DOMAIN_DEV}:${API_PORT}`;
